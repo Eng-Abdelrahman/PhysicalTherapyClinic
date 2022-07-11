347c2ef838530fe686630a78bf8328356b35d257
Abdelrahman Mahmoud <eng.abdelrahman1995@hotmail.com>
2022-07-11 11:55:26 +0200

    initial

---
+++ PhysicalTherapyClinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhysicalTherapyClinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PhysicalTherapyClinic/PTContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhysicalTherapyClinic
+{
+    public class PTContext : IdentityDbContext
+    {
+        public PTContext(DbContextOptions<PTContext> options) : base(options)
+        {
+
+        }
+    }
+}
